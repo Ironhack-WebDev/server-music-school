e72e9d1eff669fddf88e4255cf1dfffbacefa4ae
Charlotte Forsdick <c.l.forsdick@gmail.com>
2023-09-05 20:23:45 +0200

    deploy text added

--- models/Group.model.js
+++ models/Group.model.js
@@ -2,17 +2,17 @@ const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 
 const groupSchema = new Schema({
-  title: { type: String, required: true  },
+  title: { type: String, required: true },
   startTime: { type: String, required: true },
   endTime: { type: String },
   location: { type: String },
   leader: { type: String },
   imageURL: { type: String },
-  day: {type: String},
-  skillLevel: {type: String},
-  instruments: {type: Array},
-  description: {type: String},
-  members: [{ type: Schema.Types.ObjectId, ref: "User" }]
+  day: { type: String },
+  skillLevel: { type: String },
+  instruments: { type: Array },
+  description: { type: String },
+  members: [{ type: Schema.Types.ObjectId, ref: "User" }],
 });
 
 const Group = model("Group", groupSchema);
